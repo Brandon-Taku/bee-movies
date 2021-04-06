3f6b59a79c6f738e2c66d3246018890893c5a0b7
Brandon-Taku <63954524+Brandon-Taku@users.noreply.github.com>
2021-04-06 11:00:01 +0200

    Update Movie.jsx

--- src/components/Home/Movie.jsx
+++ src/components/Home/Movie.jsx
@@ -4,23 +4,6 @@ import '../card.css';
 
 const Movie = ({ title, pic, overview }) => {
 
-    const API_KEY = "fe5e020771ca617fdbe53a006489cea5";
-    const IMAGE_API = "https://image.tmdb.org/t/p/w1280";
-
-    // useEffect(() => {
-    //     getEachgMovie()
-    // }, [])
-
-    const [single, setSingle] = useState([]);
-
-    const getEachgMovie = async () => {
-        const response = await fetch(`https://api.themoviedb.org/3/movie/527774?api_key=${API_KEY}&language=en-US`);
-
-        const data = await response.json();
-        setSingle(data);
-        console.log(data);
-    }
-
     return (
 
             
